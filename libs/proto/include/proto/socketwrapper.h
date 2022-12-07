914be266f5ae3d29b3edc95ad6ecbeabbabe87b2
Yurii A <yuuurchyk@tutanota.com>
2022-12-07 23:09:22 +0000

    Add SocketWrapper class

---
+++ libs/proto/include/proto/socketwrapper.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+
+#include <boost/asio.hpp>
+#include <boost/signals2.hpp>
+
+#include "logger/logger.h"
+#include "reflection/concepts.h"
+#include "reflection/serialization.h"
+#include "utils/copymove.h"
+
+namespace Proto
+{
+/**
+ * @brief handles socket reads/writes in terms of frames
+ *
+ * When send is called, the size of frame is automatically calculated and added
+ * to the beginning of the frame (NO need to do this by hand). Same is happening
+ * on the side of reading
+ */
+class SocketWrapper : public std::enable_shared_from_this<SocketWrapper>,
+                      public logger::Entity<SocketWrapper>
+{
+    DISABLE_COPY_MOVE(SocketWrapper)
+public:
+    template <typename T>
+    using signal = boost::signals2::signal<T>;
+
+    [[nodiscard]] static std::shared_ptr<SocketWrapper>
+        create(boost::asio::io_context &, boost::asio::ip::tcp::socket socket);
+
+    void run();
+
+    void send(std::shared_ptr<Reflection::SerializationContext>);
+    template <Reflection::Serializable T>
+    void send(std::shared_ptr<const T> ptr);
+
+    signal<void(boost::asio::const_buffer)> incomingBuffer;
+    signal<void()>                          invalidated;
+
+private:
+    SocketWrapper(boost::asio::io_context &, boost::asio::ip::tcp::socket socket);
+
+    void readFrameSize();
+    void readFrame();
+
+    void invalidate();
+
+    boost::asio::io_context     &ioContext_;
+    boost::asio::ip::tcp::socket socket_;
+
+    size_t                       frameSizeBuffer_;
+    std::unique_ptr<std::byte[]> frameBuffer_;
+};
+
+}    // namespace Proto
+
+#include "detail/socketwrapper_impl.h"
---
+++ libs/proto/src/socketwrapper.cpp
@@ -0,0 +1,101 @@
+#include "proto/socketwrapper.h"
+
+#include <utility>
+
+using namespace boost::asio;
+using error_code = boost::system::error_code;
+
+namespace Proto
+{
+std::shared_ptr<SocketWrapper> SocketWrapper::create(boost::asio::io_context &ioContext,
+                                                     boost::asio::ip::tcp::socket socket)
+{
+    return std::shared_ptr<SocketWrapper>(
+        new SocketWrapper{ ioContext, std::move(socket) });
+}
+
+SocketWrapper::SocketWrapper(boost::asio::io_context     &ioContext,
+                             boost::asio::ip::tcp::socket socket)
+    : ioContext_{ ioContext }, socket_{ std::move(socket) }
+{
+}
+
+void SocketWrapper::run()
+{
+    readFrameSize();
+}
+
+void SocketWrapper::readFrameSize()
+{
+    async_read(socket_,
+               buffer(&frameSizeBuffer_, sizeof(size_t)),
+               transfer_exactly(sizeof(size_t)),
+               [this, self = shared_from_this()](const error_code &ec, size_t)
+               {
+                   if (ec)
+                   {
+                       EN_LOGE << "Failed to read from size, invalidating";
+                       return invalidate();
+                   }
+
+                   readFrame();
+               });
+}
+
+void SocketWrapper::readFrame()
+{
+    frameBuffer_.reset(new (std::nothrow) std::byte[frameSizeBuffer_]);
+
+    if (frameBuffer_ == nullptr)
+    {
+        EN_LOGE << "Failed to allocate memory for frame, invalidating";
+        return invalidate();
+    }
+
+    async_read(socket_,
+               buffer(frameBuffer_.get(), frameSizeBuffer_),
+               transfer_exactly(frameSizeBuffer_),
+               [this, self = shared_from_this()](const error_code &ec, size_t)
+               {
+                   if (ec)
+                   {
+                       EN_LOGE << "Failed to read frame, invalidating";
+                       return invalidate();
+                   }
+
+                   incomingBuffer(const_buffer(frameBuffer_.get(), frameSizeBuffer_));
+
+                   readFrameSize();
+               });
+}
+
+void SocketWrapper::invalidate()
+{
+    socket_.close();
+    invalidated();
+}
+
+void SocketWrapper::send(std::shared_ptr<Reflection::SerializationContext> context)
+{
+    if (context == nullptr)
+        return;
+
+    {
+        auto size = size_t{};
+        for (const auto &buffer : context->constBufferSequence())
+            size += buffer.size();
+
+        auto sharedSize = std::make_shared<size_t>(size);
+
+        async_write(
+            socket_,
+            const_buffer(sharedSize.get(), sizeof(size_t)),
+            [sharedSize, self = shared_from_this()](const error_code &, size_t) {});
+    }
+
+    async_write(socket_,
+                context->constBufferSequence(),
+                [context, self = shared_from_this()](const error_code &, size_t) {});
+}
+
+}    // namespace Proto
