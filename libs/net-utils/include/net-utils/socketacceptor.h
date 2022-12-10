f88afdcf40604e59633630eb2a87cf7a6278241d
Yurii A <yuuurchyk@tutanota.com>
2022-12-10 16:57:25 +0000

    Add io context selection policy to SocketAcceptor class

--- libs/net-utils/include/net-utils/socketacceptor.h
+++ libs/net-utils/include/net-utils/socketacceptor.h
@@ -13,34 +13,59 @@ namespace NetUtils
 {
 /**
  * @brief forever accepts sockets and calls new_socket_callback_fn
+ *
+ * @note the policy of io context selection can be specified during construction
+ * (i.e. by specifying the appropriate new socket callback function)
  */
-class SocketAcceptor : public std::enable_shared_from_this<SocketAcceptor>,
-                       public logger::Entity<SocketAcceptor>
+class SocketAcceptor : public std::enable_shared_from_this<SocketAcceptor>, private logger::Entity<SocketAcceptor>
 {
     DISABLE_COPY_MOVE(SocketAcceptor);
 
 public:
-    using new_socket_callback_fn = std::function<void(boost::asio::ip::tcp::socket)>;
+    struct RandomIOContextPolicy
+    {
+        using new_socket_callback_fn = std::function<void(boost::asio::ip::tcp::socket, boost::asio::io_context &)>;
+    };
+    struct LeastLoadedIOContextPolicy
+    {
+        using new_socket_callback_fn =
+            std::function<void(boost::asio::ip::tcp::socket, std::unique_ptr<IOContextPool::LoadGuard>)>;
+    };
 
-    static std::shared_ptr<SocketAcceptor>
-        create(boost::asio::io_context &acceptorContext,
-               unsigned short           port,
-               new_socket_callback_fn,
-               IOContextPool &workersPool);
+    [[nodiscard]] static std::shared_ptr<SocketAcceptor> create(
+        boost::asio::io_context                      &acceptorContext,
+        unsigned short                                port,
+        RandomIOContextPolicy::new_socket_callback_fn newSocketCallback,
+        std::shared_ptr<IOContextPool>                workersPool);
+    [[nodiscard]] static std::shared_ptr<SocketAcceptor> create(
+        boost::asio::io_context                           &acceptorContext,
+        unsigned short                                     port,
+        LeastLoadedIOContextPolicy::new_socket_callback_fn newSocketCallback,
+        std::shared_ptr<IOContextPool>                     workersPool);
 
     void run();
 
 private:
-    SocketAcceptor(boost::asio::io_context &,
-                   unsigned short port,
-                   new_socket_callback_fn,
-                   IOContextPool &);
+    SocketAcceptor(
+        boost::asio::io_context &,
+        unsigned short port,
+        RandomIOContextPolicy::new_socket_callback_fn,
+        std::shared_ptr<IOContextPool>);
+    SocketAcceptor(
+        boost::asio::io_context &,
+        unsigned short port,
+        LeastLoadedIOContextPolicy::new_socket_callback_fn,
+        std::shared_ptr<IOContextPool>);
 
-    void acceptOne();
+    void acceptOne(RandomIOContextPolicy);
+    void acceptOne(LeastLoadedIOContextPolicy);
 
     boost::asio::ip::tcp::acceptor acceptor_;
-    new_socket_callback_fn         newSocketCallback_;
-    IOContextPool                 &workersPool_;
+
+    RandomIOContextPolicy::new_socket_callback_fn      randomCallback_{};
+    LeastLoadedIOContextPolicy::new_socket_callback_fn leastLoadedCallback_{};
+
+    std::shared_ptr<IOContextPool> workersPool_;
 };
 
 }    // namespace NetUtils
--- libs/net-utils/src/socketacceptor.cpp
+++ libs/net-utils/src/socketacceptor.cpp
@@ -7,54 +7,109 @@ using error_code = boost::system::error_code;
 
 namespace NetUtils
 {
-std::shared_ptr<SocketAcceptor>
-    SocketAcceptor::create(io_context            &acceptorContext,
-                           unsigned short         port,
-                           new_socket_callback_fn newSocketCallback,
-                           IOContextPool         &workersPool)
+std::shared_ptr<SocketAcceptor> SocketAcceptor::create(
+    io_context                                   &acceptorContext,
+    unsigned short                                port,
+    RandomIOContextPolicy::new_socket_callback_fn newSocketCallback,
+    std::shared_ptr<IOContextPool>                workers)
 {
     return std::shared_ptr<SocketAcceptor>{ new SocketAcceptor{
-        acceptorContext, port, std::move(newSocketCallback), workersPool } };
+        acceptorContext, port, std::move(newSocketCallback), std::move(workers) } };
+}
+std::shared_ptr<SocketAcceptor> SocketAcceptor::create(
+    io_context                                        &acceptorContext,
+    unsigned short                                     port,
+    LeastLoadedIOContextPolicy::new_socket_callback_fn newSocketCallback,
+    std::shared_ptr<IOContextPool>                     workers)
+{
+    return std::shared_ptr<SocketAcceptor>{ new SocketAcceptor{
+        acceptorContext, port, std::move(newSocketCallback), std::move(workers) } };
+}
+
+SocketAcceptor::SocketAcceptor(
+    io_context                                   &acceptorContext,
+    unsigned short                                port,
+    RandomIOContextPolicy::new_socket_callback_fn newSocketCallback,
+    std::shared_ptr<IOContextPool>                workersPool)
+    : acceptor_{ acceptorContext, ip::tcp::endpoint{ ip::tcp::v4(), port } },
+      randomCallback_{ std::move(newSocketCallback) },
+      workersPool_{ std::move(workersPool) }
+{
+}
+SocketAcceptor::SocketAcceptor(
+    io_context                                        &acceptorContext,
+    unsigned short                                     port,
+    LeastLoadedIOContextPolicy::new_socket_callback_fn newSocketCallback,
+    std::shared_ptr<IOContextPool>                     workersPool)
+    : acceptor_{ acceptorContext, ip::tcp::endpoint{ ip::tcp::v4(), port } },
+      leastLoadedCallback_{ std::move(newSocketCallback) },
+      workersPool_{ std::move(workersPool) }
+{
 }
 
 void SocketAcceptor::run()
 {
-    acceptOne();
+    if (randomCallback_ != nullptr)
+        return acceptOne(RandomIOContextPolicy{});
+    if (leastLoadedCallback_ != nullptr)
+        return acceptOne(LeastLoadedIOContextPolicy{});
+
+    EN_LOGE << "No suitable policy to choose on run()";
 }
 
-SocketAcceptor::SocketAcceptor(io_context            &acceptorContext,
-                               unsigned short         port,
-                               new_socket_callback_fn newSocketCallback,
-                               IOContextPool         &workersPool)
-    : acceptor_{ acceptorContext, ip::tcp::endpoint{ ip::tcp::v4(), port } },
-      newSocketCallback_{ newSocketCallback == nullptr ? [](ip::tcp::socket) {} :
-                                                         std::move(newSocketCallback) },
-      workersPool_{ workersPool }
+void SocketAcceptor::acceptOne(RandomIOContextPolicy)
 {
-}
+    auto &ioContext = workersPool_->getNext();
 
-void SocketAcceptor::acceptOne()
-{
-    auto  socketOwner = std::make_unique<ip::tcp::socket>(workersPool_.getNext());
+    auto  socketOwner = std::make_unique<ip::tcp::socket>(ioContext);
     auto &socket      = *socketOwner;
 
-    acceptor_.async_accept(socket,
-                           [this,
-                            self        = shared_from_this(),
-                            socketOwner = std::move(socketOwner)](const error_code &error)
-                           {
-                               if (error)
-                               {
-                                   EN_LOGE << "Could not accept incoming connection";
-                                   return;
-                               }
+    acceptor_.async_accept(
+        socket,
+        [this, &ioContext, socketOwner = std::move(socketOwner), self = shared_from_this()](const error_code &ec)
+        {
+            if (ec)
+            {
+                EN_LOGE << "Could not accept incoming connection";
+                return;
+            }
 
-                               EN_LOGI << "Accepting incoming connection";
+            EN_LOGI << "Accepting incoming connection";
 
-                               newSocketCallback_(std::move(*socketOwner));
+            randomCallback_(std::move(*socketOwner), ioContext);
 
-                               acceptOne();
-                           });
+            acceptOne(RandomIOContextPolicy{});
+        });
+}
+
+void SocketAcceptor::acceptOne(LeastLoadedIOContextPolicy)
+{
+    auto  loadGuard = workersPool_->getLeastLoaded();
+    auto &ioContext = loadGuard->ioContext_;
+
+    auto  socketOwner = std::make_unique<ip::tcp::socket>(loadGuard->ioContext_);
+    auto &socket      = *socketOwner;
+
+    acceptor_.async_accept(
+        socket,
+        [this,
+         &ioContext,
+         socketOwner = std::move(socketOwner),
+         loadGuard   = std::move(loadGuard),
+         self        = shared_from_this()](const error_code &ec) mutable
+        {
+            if (ec)
+            {
+                EN_LOGE << "Could not accept incoming conection";
+                return;
+            }
+
+            EN_LOGI << "Accepting incoming connection";
+
+            leastLoadedCallback_(std::move(*socketOwner), std::move(loadGuard));
+
+            acceptOne(LeastLoadedIOContextPolicy{});
+        });
 }
 
 }    // namespace NetUtils
