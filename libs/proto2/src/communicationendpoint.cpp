d6545e0a5125685ea1995aa9ecb7c70fc27e82be
Yurii A <yuuurchyk@tutanota.com>
2023-01-06 23:27:43 +0000

    Add draft for outcoming requests manager

---
+++ libs/proto2/src/communicationendpoint.cpp
@@ -0,0 +1,7 @@
+#include "proto2/communicationendpoint.h"
+
+namespace Proto2
+{
+CommunicationEndpoint::~CommunicationEndpoint() = default;
+
+}    // namespace Proto2
---
+++ libs/proto2/src/outcomingrequestsmanager.cpp
@@ -0,0 +1,33 @@
+#include "outcomingrequestsmanager.h"
+
+#include <utility>
+
+namespace Proto2
+{
+
+std::unique_ptr<OutcomingRequestsManager>
+    OutcomingRequestsManager::create(std::shared_ptr<boost::asio::ip::tcp::socket> socket, size_t responseTimeoutMs)
+{
+    return std::unique_ptr<OutcomingRequestsManager>{ new OutcomingRequestsManager{ std::move(socket),
+                                                                                    responseTimeoutMs } };
+}
+
+void OutcomingRequestsManager::send(
+    std::shared_ptr<Request::AbstractRequest>                                 request,
+    std::shared_ptr<OutcomingRequestContext::AbstractOutcomingRequestContext> requestContext)
+{
+    if (request == nullptr)
+    {
+        EN_LOGW << "request should not be nullptr, not sending request";
+        return;
+    }
+    if (requestContext == nullptr)
+    {
+        EN_LOGW << "requestContext should not be nullptr, not sending request";
+        return;
+    }
+
+    const auto requestId = getNextRequestId();
+}
+
+}    // namespace Proto2
