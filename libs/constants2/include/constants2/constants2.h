87aa7737e169ee2194c0ca27876b0cd6cccc1c4f
Yurii A <yuuurchyk@tutanota.com>
2023-01-10 03:53:05 +0000

    Add secondary node main.cpp code

--- libs/constants2/include/constants2/constants2.h
+++ libs/constants2/include/constants2/constants2.h
@@ -8,10 +8,13 @@ namespace Constants2
 {
 using duration_milliseconds_t = std::chrono::duration<size_t, std::milli>;
 
-static constexpr duration_milliseconds_t                                     kOutcomingRequestTimeout{ 1500 };
-static constexpr std::pair<duration_milliseconds_t, duration_milliseconds_t> kArtificialSendDelayBounds{
+constexpr duration_milliseconds_t                                     kOutcomingRequestTimeout{ 1500 };
+constexpr std::pair<duration_milliseconds_t, duration_milliseconds_t> kArtificialSendDelayBounds{
     duration_milliseconds_t{ 1000 },
     duration_milliseconds_t{ 2000 }
 };
+
+// secondary specific
+constexpr duration_milliseconds_t kMasterReconnectTimeout{ 3000 };
 
 }    // namespace Constants2
